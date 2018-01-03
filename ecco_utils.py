5c45c14fd2166393e02aa08c5ea7b499e78ac8e9
Jan-Erik Tesdal <je.tesdal@gmail.com>
2018-01-03 11:26:37 +0100

    updated base_dir requirements

--- ecco_utils.py
+++ ecco_utils.py
@@ -26,6 +26,52 @@ import os
 import numpy as np 
 import xarray as xr
 
+def open_ecco_grid(grid_dir=None):
+    if grid_dir is None:
+        grid_dir = '/nctiles_grid/'
+    grid = xr.open_mfdataset(grid_dir + 'GRID.*.nc', concat_dim='face')
+    # Renaming dimensions to match data variables
+    grid = grid.rename({'i3': 'i4'}).rename({'i2': 'i3'}).rename({'i1': 'i2'})
+    return grid
+
+def open_ecco_single_variable(base_dir=None, varname):
+    if base_dir is None:
+        base_dir = '/nctiles_monthly'
+    ds = xr.open_mfdataset(os.path.join(base_dir, varname, '*.nc'),concat_dim='face')
+    # make time an actual dimension
+    if 'tim' in ds:
+        tdim = ds['tim'].dims[0]
+        ds = ds.swap_dims({tdim: 'tim'})
+        ds = ds.rename({'tim': 'time'})
+    # make sure that the dimension names are consistent
+    if ds['area'].dims == ('face', 'i2', 'i3'):
+        # we probably have a 2D field
+        ds = ds.rename({'i3': 'i4'}).rename({'i2': 'i3'})
+    ds = ds.reset_coords()
+    da = ds[varname]
+    dims = list(da.dims)
+    # possibly transpose
+    if 'time' in dims:
+        if dims[0] != 'time':
+            newdims = [d for d in dims]
+            newdims[0] = 'time'
+            newdims[1] = dims[0]
+            da = da.transpose(*newdims)
+    return da
+
+def open_ecco_variables(ncdir, grid_dir=None, variables=None):
+    if variables is None:
+        variables = []
+        for fname in os.listdir(ncdir):
+            if os.path.isdir(os.path.join(ncdir,fname)):
+                variables.append(fname)
+    darrays = [open_ecco_single_variable(v, ncdir) for v in variables]
+    if not grid_dir is None:
+        grid = open_ecco_grid(grid_dir)
+        grid = grid.set_coords(grid.data_vars)
+        darrays.append(grid)
+    return xr.merge(darrays)
+
 def open_ecco_single_tendency(varname, dirname, base_dir):
     ds = xr.open_mfdataset(os.path.join(base_dir, dirname, '*.nc'),concat_dim='face')
 
@@ -71,64 +117,20 @@ def open_ecco_single_tendency(varname, dirname, base_dir):
     
     return ds
 
-def open_ecco_tendencies(*varnames, **keyword_parameters):
-    base_dir = '/mnt/efs/data/ecco_v4/nctiles_tendencies'
+def open_ecco_tendencies(base_dir=None, *varnames, **keyword_parameters):
+    if base_dir is None:
+        base_dir = '/nctiles_tendencies'
     if ('base_dir' in keyword_parameters):
         base_dir = keyword_parameters['base_dir']
     darrays = [open_ecco_single_tendency(v, v, base_dir) for v in varnames]
     return xr.merge(darrays)
 
-def open_ecco_snapshots(*varnames, **keyword_parameters):
-    base_dir = '/mnt/efs/data/ecco_v4/nctiles_tendencies/'
+def open_ecco_snapshots(base_dir=None, *varnames, **keyword_parameters):
+    if base_dir is None:
+        base_dir = '/nctiles_monthly_snapshots'
     if ('base_dir' in keyword_parameters):
         base_dir = keyword_parameters['base_dir']
     darrays = [open_ecco_single_tendency(v,
                     'Ssnapshot' if v == 'SALT' else 'Tsnapshot',
                     base_dir) for v in varnames]
-    return xr.merge(darrays)
-
-def open_ecco_grid(grid_dir=None):
-    if grid_dir is None:
-        grid_dir = '/nctiles_grid/'
-    grid = xr.open_mfdataset(grid_dir + 'GRID.*.nc', concat_dim='face')
-    # Renaming dimensions to match data variables
-    grid = grid.rename({'i3': 'i4'}).rename({'i2': 'i3'}).rename({'i1': 'i2'})
-    return grid
-
-def open_ecco_single_variable(varname, base_dir=None):
-    if base_dir is None:
-        base_dir = '/nctiles_monthly'
-    ds = xr.open_mfdataset(os.path.join(base_dir, varname, '*.nc'),concat_dim='face')
-    # make time an actual dimension
-    if 'tim' in ds:
-        tdim = ds['tim'].dims[0]
-        ds = ds.swap_dims({tdim: 'tim'})
-        ds = ds.rename({'tim': 'time'})
-    # make sure that the dimension names are consistent
-    if ds['area'].dims == ('face', 'i2', 'i3'):
-        # we probably have a 2D field
-        ds = ds.rename({'i3': 'i4'}).rename({'i2': 'i3'})
-    ds = ds.reset_coords()
-    da = ds[varname]
-    dims = list(da.dims)
-    # possibly transpose
-    if 'time' in dims:
-        if dims[0] != 'time':
-            newdims = [d for d in dims]
-            newdims[0] = 'time'
-            newdims[1] = dims[0]
-            da = da.transpose(*newdims)
-    return da
-
-def open_ecco_variables(ncdir, grid_dir=None, variables=None):
-    if variables is None:
-        variables = []
-        for fname in os.listdir(ncdir):
-            if os.path.isdir(os.path.join(ncdir,fname)):
-                variables.append(fname)
-    darrays = [open_ecco_single_variable(v, ncdir) for v in variables]
-    if not grid_dir is None:
-        grid = open_ecco_grid(grid_dir)
-        grid = grid.set_coords(grid.data_vars)
-        darrays.append(grid)
     return xr.merge(darrays)
